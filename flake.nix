2fe0297f3db2b4d9f4d2b5f06854b1489c6e5861
Elis Hirwing <elis@hirwing.se>
2023-10-17 21:28:16 +0200

    feat: Update flake to align with the updated upstream template

--- flake.nix
+++ flake.nix
@@ -1,12 +1,9 @@
 {
   description = "Your personal jsonresume built with Nix";
 
-  inputs = {
-    jsonresume-nix.url = "github:TaserudConsulting/jsonresume-nix";
-    jsonresume-nix.inputs.flake-utils.follows = "flake-utils";
-
-    flake-utils.url = "flake-utils";
-  };
+  inputs.jsonresume-nix.url = "github:TaserudConsulting/jsonresume-nix";
+  inputs.jsonresume-nix.inputs.flake-utils.follows = "flake-utils";
+  inputs.flake-utils.url = "flake-utils";
 
   outputs = {
     jsonresume-nix,
@@ -14,23 +11,40 @@
     flake-utils,
     nixpkgs,
     ...
-  }:
+  } @ inputs:
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = nixpkgs.legacyPackages.${system};
+      lib = pkgs.lib;
     in {
       # Specify formatter package for "nix fmt ." and "nix fmt . -- --check"
       formatter = pkgs.alejandra;
 
-      # Specify default package to use to build your resume, this will
-      # decide which theme to use.
+      # Specify the builder package to use to build your resume, this
+      # will decide which theme to use.
       #
       # To show available packaged themes:
       # nix flake show github:TaserudConsulting/jsonresume-nix
       #
       # If you miss a theme, consider opening a pull request :)
       packages = {
-        default = jsonresume-nix.packages.${system}.resumed-fullmoon;
-        inherit (jsonresume-nix.packages.${system}) nix-to-json;
+        builder = jsonresume-nix.packages.${system}.resumed-fullmoon;
+        inherit (jsonresume-nix.packages.${system}) fmt-as-json;
       };
-    });
+
+      # Allows to run a live preview server using "nix run .#live"
+      apps = {
+        live.type = "app";
+        live.program = builtins.toString (pkgs.writeShellScript "entr-reload" ''
+          ${self.packages.${system}.builder}
+
+          ${lib.getExe pkgs.nodePackages.live-server} \
+            --watch=resume.html --open=resume.html --wait=300 &
+
+          printf "\n%s" resume.{toml,nix,json} |
+            ${lib.getExe pkgs.xe} -s 'test -f "$1" && echo "$1"' |
+            ${lib.getExe pkgs.entr} -p ${self.packages.${system}.builder}
+        '');
+      };
+    })
+    // {inherit inputs;};
 }
