fc17a174e586b836724351da3679883fae77f10e
Elis Hirwing <elis@hirwing.se>
2024-10-04 18:15:07 +0200

    feat: Major cleanup

--- resume.nix
+++ resume.nix
@@ -8,7 +8,17 @@
     email = "elis@hirwing.se";
     phone = "+46 70 452 12 98";
     url = "https://elis.nu";
-    summary = "I'm a DevOps engineer who can manage everything from backend API development. Managing operating systems. Backups and restoration. Even hardware purchases for on-premises hardware.";
+    summary = ''
+      I'm a DevOps and Backend Engineer with a proven track record of building
+      scalable infrastructures and optimizing backend systems.
+
+      With over a decade of experience across various tech stacks, I've led
+      significant projects, built high-availability systems, and helped teams
+      transition to modern architectures.
+
+      I'm passionate about open-source, containerization, and continuous
+      improvement in all facets of development.
+    '';
     location = {
       address = "Repslagaregatan 20A";
       postalCode = "67130";
@@ -56,7 +66,15 @@
       position = "Founder / Consultant";
       url = "https://taserud.net";
       startDate = "2023-08-02";
-      summary = "Founder of my own consulting company to be able to do consulting by the side of my job at TV.nu.";
+      summary = ''
+        I founded Taserud Consulting to provide specialized consulting
+        services in backend development and DevOps with a focus on NixOS
+        if the customer is open to that choice. Otherwise I'll work with
+        any Linux distribution.
+
+        I'm also working with backups, reproducibe builds, CI/CD pipelines,
+        high availability, security through reproducibiity and testability.
+      '';
       highlights = [];
     }
     {
@@ -65,43 +83,77 @@
       position = "Senior / Lead Backend Developer";
       url = "https://tv.nu";
       startDate = "2016-04";
-      summary = "Started as Senior Backend Developer at TV.nu, the biggest TV and VOD-Guide in Sweden. The Backend Team is responsible for server maintenance, development of the API's for the clients and ingestion of both linear TV data and VOD-data from different third parties.";
+      summary = ''
+        I started at TV.nu as a Senior Backend Developer and progressed
+        into a Lead role. My team is responsible for maintaining the
+        core API and infrastructure, ensuring high availability, and
+        ingesting data from multiple third-party sources.
+
+        I also led key migrations to modernize the backend and improve
+        system performance.
+      '';
       highlights = [
-        "Responsible to set up HAProxy in front of the site with TLS termination to introduce HTTPS."
-        "Upgraded from PHP 5.5 to 7.1."
-        "Introduced the modular start page API with client capabilities to choose what to show on different clients."
-        "Upgraded from PHP 7.1 to 7.3."
-        "Upgraded from PHP 7.3 to 8.1."
-        "Migrated to Docker."
-        "Migrated infrastructure from classical hosting with PHP and Nginx to on-premises Kubernetes."
-        "Upgraded Percona (MySQL) 5.6 to MariaDB 10.6 without downtime."
+        ''
+          Introduced TLS termination and HTTPS with HAProxy, securing user
+          data and improving trust and high availability entry to the systems.
+        ''
+        ''
+          Upgraded the entire system from PHP 5.5 to 8.1, resulting
+          in significant performance improvements.
+        ''
+        ''
+          Pioneered a modular front page API to allow for different data
+          types to be rendered depening on user selection and client
+          capabilities to render different versions of different content.
+        ''
+        ''
+          Implemented Docker across all environments to streamline
+          development and deployment.
+        ''
+        "Led migration from legacy infrastructure to containers on Kubernetes."
+        ''
+          Managed a zero-downtime migration of MySQL to MariaDB 10.6,
+          improving database performance and scalability.
+        ''
       ];
     }
     {
       name = "Shopello International AB / 203 Brands AB";
       location = "Stockholm";
-      position = "Web developer for Shopello";
+      position = "Web Developer at Shopello";
       url = "https://shopello.se";
       startDate = "2014-04";
       endDate = "2016-04";
-      summary = "My primary role were to work on Shopello.se which is a shopping search engine with an API for bartners. We worked a lot with ingestion of huge amounts of XML and CSV in PHP.";
+      summary = ''
+        At Shopello, I developed and maintained a shopping search engine,
+        handling ingestion of large datasets and working on API
+        improvements to better handle high traffic.
+      '';
       highlights = [
-        "I pioneered a new faster API that used signed data rather than unique entries in a database to be able to generate trusted links on the fly in the API to scale it better."
-        "I introduced streaming XML files rather than reading them in full."
+        ''
+          Pioneered a new API for scaling faster while maintaining
+          data integrity with signed links.
+        ''
+        "Introduced XML file streaming to handle large datasets more efficiently."
       ];
     }
     {
       name = "Tailor Store Sweden AB";
       location = "Helsingborg";
-      position = "Web developer / Server admin";
+      position = "Web Developer / Server Admin";
       url = "https://www.tailorstore.com/";
       startDate = "2011-11";
       endDate = "2014-04";
-      summary = "My primary role were development, bugfixing and maintenance of the main homepage and admin systems. I also maintained the servers and the office network.";
+      summary = ''
+        I maintained the main website and admin systems, along with
+        managing servers and the office network. My contributions
+        played a key role in improving the overall system reliability
+        and performance.
+      '';
       highlights = [
-        "Key role in purchases of new servers on several occasions."
-        "Key role in designing the flexible product design API that is still in use."
-        "Upgraded from PHP 5.3 to 5.4."
+        "Led purchases and setup of new server infrastructure with HAProxy for high availability."
+        "Designed a flexible product design API that’s still in use today."
+        "Handled upgrades from PHP 5.3 to 5.4, ensuring smooth transitions with no downtime."
       ];
     }
     {
@@ -111,7 +163,10 @@
       url = "https://southpole.se";
       startDate = "2011-09";
       endDate = "2011-11";
-      summary = "Short time contract to assemble computer clusters for universities in Sweden with more than 400 computers per cluster.";
+      summary = ''
+        Short time contract to assemble computer clusters for universities
+        in Sweden with more than 400 computers per cluster.
+      '';
     }
     {
       name = "Webhallen Sverige AB";
@@ -120,7 +175,12 @@
       url = "https://webhallen.com";
       startDate = "2011-01";
       endDate = "2011-08";
-      summary = "Most of my work were to do website maintenance and bugfixes. I also did maintenance of internal admin systems.";
+      summary = ''
+        I maintained the main website and admin systems, along with
+        managing servers and the office network. My contributions
+        played a key role in improving the overall system reliability
+        and performance.
+      '';
       highlights = [
         "Key role in implementing imports of data sheets from external systems."
         "Key role in implementing DLC product keys for game purchases."
@@ -132,7 +192,11 @@
       position = "IT-Support / Web Admin / Web Developer";
       startDate = "2010-03";
       endDate = "2010-11";
-      summary = "Most of my work was to answer phone calls to support the software in a Windows environment. I were also part of bootstrapping the development of a new web-based project for future endeavors.";
+      summary = ''
+        Most of my work was to answer phone calls to support the software
+        in a Windows environment. I were also part of bootstrapping the
+        development of a new web-based project for future endeavors.
+      '';
     }
   ];
 
@@ -149,34 +213,34 @@
       startDate = "2022";
       summary = "Vice-President of the board of the association that owns the buildings I live in.";
     }
-    {
-      organization = "Unga Forskare Stockholm";
-      position = "Board Member / Auditor";
-      startDate = "2016";
-      endDate = "2019";
-      summary = "Some years I was an auditor of the board and some other years I was part of the board.";
-    }
-    {
-      organization = "Stockholm Linux User Group";
-      position = "Organizer";
-      startDate = "2010";
-      endDate = "2011";
-      summary = "Responsible for making sure Proxxi was opened so Stockholm Linux User Group could meet up there every Monday.";
-    }
-    {
-      organization = "Sverok";
-      position = "Server team";
-      startDate = "2010";
-      endDate = "2011";
-      summary = "Participated in the server team's work this year, I learned much from but didn't help out much during this time. The Server team maintains the organizations servers and the technical questions from member organizations.";
-    }
-    {
-      organization = "Konventare.se";
-      position = "Founder / President / Developer";
-      startDate = "2008";
-      endDate = "2010";
-      summary = "Founded Konventare.se where some friends and I listed most Games and Anime conventions in Sweden to make one go-to place for people to find them.";
-    }
+    #{
+    #  organization = "Unga Forskare Stockholm";
+    #  position = "Board Member / Auditor";
+    #  startDate = "2016";
+    #  endDate = "2019";
+    #  summary = "Some years I was an auditor of the board and some other years I was part of the board.";
+    #}
+    #{
+    #  organization = "Stockholm Linux User Group";
+    #  position = "Organizer";
+    #  startDate = "2010";
+    #  endDate = "2011";
+    #  summary = "Responsible for making sure Proxxi was opened so Stockholm Linux User Group could meet up there every Monday.";
+    #}
+    #{
+    #  organization = "Sverok";
+    #  position = "Server team";
+    #  startDate = "2010";
+    #  endDate = "2011";
+    #  summary = "Participated in the server team's work this year, I learned much from but didn't help out much during this time. The Server team maintains the organizations servers and the technical questions from member organizations.";
+    #}
+    #{
+    #  organization = "Konventare.se";
+    #  position = "Founder / President / Developer";
+    #  startDate = "2008";
+    #  endDate = "2010";
+    #  summary = "Founded Konventare.se where some friends and I listed most Games and Anime conventions in Sweden to make one go-to place for people to find them.";
+    #}
     #{
     #  organization = "Sverok";
     #  position = "Representative at Annual meeting";
@@ -184,13 +248,13 @@
     #  endDate = "2010";
     #  summary = "Represented several different non-profit organizations interests at the big annual meeting.";
     #}
-    {
-      organization = "Proxxi";
-      position = "Board member";
-      startDate = "2008";
-      endDate = "2010";
-      summary = "Proxxi used to be a non-profit location for computer/hacking/computer games and board games that were around since early 1980's, sadly the space closed down in 2012 due to an unfortunate series of events and never managed to recover the active members.";
-    }
+    #{
+    #  organization = "Proxxi";
+    #  position = "Board member";
+    #  startDate = "2008";
+    #  endDate = "2010";
+    #  summary = "Proxxi used to be a non-profit location for computer/hacking/computer games and board games that were around since early 1980's, sadly the space closed down in 2012 due to an unfortunate series of events and never managed to recover the active members.";
+    #}
     #{
     #  organization = "Röda Handen";
     #  position = "Founder";
@@ -201,39 +265,39 @@
   ];
 
   education = [
-    {
-      # Location: Göteborg, institution isn't known
-      area = "CE Certification of software in medicine technical products";
-      studyType = "Two-day Course";
-      # Not sure really, it was in fall of 2010 over a couple of days
-      startDate = "2010-10";
-      endDate = "2010-10";
-      score = "Passed";
-    }
-    {
-      institution = "SK0̷QO";
-      area = "Amateur radio certification";
-      studyType = "Weekend-course";
-      # Not sure really, it was in spring of 2010 over a couple of weekends
-      startDate = "2010-03";
-      endDate = "2010-04";
-      score = "Passed";
-      courses = [
-        "Technical and safety"
-        "Regulation"
-      ];
-    }
-    {
-      institution = "Stockholms Universitet";
-      area = "Mathematics";
-      studyType = "Full-time";
-      startDate = "2009-01";
-      endDate = "2009-02";
-      score = "Did not complete";
-      courses = [
-        "Mathematics"
-      ];
-    }
+    #{
+    #  # Location: Göteborg, institution isn't known
+    #  area = "CE Certification of software in medicine technical products";
+    #  studyType = "Two-day Course";
+    #  # Not sure really, it was in fall of 2010 over a couple of days
+    #  startDate = "2010-10";
+    #  endDate = "2010-10";
+    #  score = "Passed";
+    #}
+    #{
+    #  institution = "SK0̷QO";
+    #  area = "Amateur Radio Certification";
+    #  studyType = "Weekend-course";
+    #  # Not sure really, it was in spring of 2010 over a couple of weekends
+    #  startDate = "2010-03";
+    #  endDate = "2010-04";
+    #  score = "Passed";
+    #  courses = [
+    #    "Technical and safety"
+    #    "Regulation"
+    #  ];
+    #}
+    #{
+    #  institution = "Stockholms Universitet";
+    #  area = "Mathematics";
+    #  studyType = "Full-time";
+    #  startDate = "2009-01";
+    #  endDate = "2009-02";
+    #  score = "Did not complete";
+    #  courses = [
+    #    "Mathematics"
+    #  ];
+    #}
     {
       institution = "S:t Eriks Gymnasium";
       area = "Technical, Computers, Physics and Math program";
@@ -272,56 +336,44 @@
 
   skills = [
     {
-      name = "Web Development";
+      name = "DevOps";
       level = "Master";
       keywords = [
-        "PHP"
-        "HTML"
-        "CSS"
-        "SQL"
-        "MySQL"
-        "PostgresQL"
-        "Redis"
-        "Memcache"
-        "Docker"
-      ];
-    }
-    {
-      name = "Operating systems";
-      level = "Master";
-      keywords = [
-        "NixOS"
-        "Docker"
-        "Ubuntu"
-        "Arch Linux"
-        "Red Hat"
-        "Gentoo"
-      ];
-    }
-    {
-      name = "Systems Programming";
-      level = "Master";
-      keywords = [
-        "Nix"
-        "Shell (sh)"
-        "Shell (bash)"
-        "Shell (fish)"
-        "Go"
-        "Python"
-      ];
-    }
-    {
-      name = "Kubernetes";
-      level = "Master";
-      keywords = [
-        "Self-managed on-prem Kubernets"
-        "Helm"
         "Ansible"
-        "Kubespray"
-        "Monitoring"
-        "Prometheus"
+        "CI/CD"
         "Grafana"
-        "InfluxDB"
+        "Helm"
+        "Jenkins"
+        "Kubernetes"
+        "Nix"
+        "Prometheus"
+      ];
+    }
+    {
+      name = "System Administration";
+      level = "Master";
+      keywords = [
+        "Backups"
+        "Docker"
+        "Linux"
+        "NixOS"
+        "Server management"
+        "Ubuntu"
+        "ZFS"
+      ];
+    }
+    {
+      name = "Backend Development";
+      level = "Master";
+      keywords = [
+        "Bash"
+        "Docker"
+        "Go"
+        "MariaDB"
+        "PHP"
+        "PostgreSQL"
+        "Redis"
+        "SQL"
       ];
     }
   ];
@@ -339,21 +391,22 @@
 
   interests = [
     {
-      name = "Free software";
+      name = "Free Software";
       keywords = [
-        "Linux"
-        "Software Freedom"
         "NixOS"
+        "Linux"
+        "Free And Open-Source Software Community"
+        "Free And Open-Source Software Development"
         "Emacs"
       ];
     }
     {
-      name = "Health and fitness";
+      name = "Health and Fitness";
       keywords = [
-        "Eating"
-        "Fitness"
-        "Mental health"
-        "Weight management"
+        "Nutrition"
+        "Mental well-being"
+        "Running"
+        "Climbing"
       ];
     }
   ];
@@ -367,29 +420,24 @@
 
   projects = [
     {
-      name = "NixOS advocate";
-      description = "Been using NixOS since 2017 and been a package maintainer since 2018. I've been to the community conference once and would have liked to been there more times. However, due to circumstances with other responsibilites that haven't been possible.";
+      name = "NixOS Advocate";
+      description = ''
+        I’ve been an active contributor to the NixOS community since 2017,
+        maintaining packages and advocating for the use of NixOS in
+        production environments.
+
+        I've also authored blog posts and contributed to modular PHP
+        builds and bootstrapped overlays like the Emacs overlay.
+      '';
       highlights = [
-        "Part of modular PHP builds."
-        "Part of bootstrapping of the emacs overlay."
-        "Part of the creation of of impermanence project."
-        "Author of the well known blog post 'NixOS ❄: tmpfs as root'."
-        "Motivation and support to get support to build PHP composer packages in nixpkgs."
-      ];
-      keywords = [
-        "PHP"
-        "composer"
-        "ZFS"
-        "Operating systems"
-        "Deployments"
+        "Maintained core PHP packages."
+        "Contributed to the Emacs overlay project."
+        "Published the well-known blog post 'NixOS ❄: tmpfs as root'."
+        "Written about ZFS and backups."
       ];
       startDate = "2017-07-08";
       # endDate = "Not done yet!";
       # url = "https://example.org";
-      roles = [
-        "Package maintainer"
-        "User"
-      ];
       entity = "NixOS";
       type = "maintainer";
     }
